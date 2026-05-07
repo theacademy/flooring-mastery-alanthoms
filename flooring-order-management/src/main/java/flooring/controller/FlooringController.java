30dc782b7a20acb999bcb84c15ed5c22fbf35468
Alan Thomas <alanthomas171@gmail.com>
2026-05-07 14:18:41 +0100

    Commit 2/ Feat/ UserIO Interface and Impl

---
+++ flooring-order-management/src/main/java/flooring/controller/FlooringController.java
@@ -0,0 +1,48 @@
+package flooring.controller;
+
+
+import flooring.view.UserIO;
+import flooring.view.UserIOConsoleImpl;
+
+public class FlooringController {
+
+    private UserIO io = new UserIOConsoleImpl();
+
+    public void run() {
+        boolean keepGoing = true;
+        int menuSelection = 0;
+        while (keepGoing) {
+            io.print("Main Menu");
+            io.print("1. List Student IDs");
+            io.print("2. Create New Student");
+            io.print("3. View a Student");
+            io.print("4. Remove a Student");
+            io.print("5. Exit");
+
+            menuSelection = io.readInt("Please select from the"
+                    + " above choices.", 1, 5);
+
+            switch (menuSelection) {
+                case 1:
+                    io.print("LIST STUDENTS");
+                    break;
+                case 2:
+                    io.print("CREATE STUDENT");
+                    break;
+                case 3:
+                    io.print("VIEW STUDENT");
+                    break;
+                case 4:
+                    io.print("REMOVE STUDENT");
+                    break;
+                case 5:
+                    keepGoing = false;
+                    break;
+                default:
+                    io.print("UNKNOWN COMMAND");
+            }
+
+        }
+        io.print("GOOD BYE");
+    }
+}
