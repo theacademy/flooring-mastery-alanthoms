c0cf6c16092e0800514a628f5ccac417a1127494
Alan Thomas <alanthomas171@gmail.com>
2026-05-07 14:21:40 +0100

    Commit 3/ Feat/ Controller and App

--- flooring-order-management/src/main/java/App.java
+++ flooring-order-management/src/main/java/App.java
@@ -1,2 +1,8 @@
+import flooring.controller.FlooringController;
+
 public class App {
+    public static void main(String[] args) {
+        FlooringController controller = new FlooringController();
+        controller.run();
+    }
 }
--- flooring-order-management/src/main/java/flooring/controller/FlooringController.java
+++ flooring-order-management/src/main/java/flooring/controller/FlooringController.java
@@ -13,29 +13,33 @@ public class FlooringController {
         int menuSelection = 0;
         while (keepGoing) {
             io.print("Main Menu");
-            io.print("1. List Student IDs");
-            io.print("2. Create New Student");
-            io.print("3. View a Student");
-            io.print("4. Remove a Student");
-            io.print("5. Exit");
+            io.print("1. Display Orders");
+            io.print("2. Add an Order");
+            io.print("3. Edit an Order ");
+            io.print("4. Remove an Order");
+            io.print("5. Export all Data ");
+            io.print("6. Exit");
 
             menuSelection = io.readInt("Please select from the"
-                    + " above choices.", 1, 5);
+                    + " above choices.", 1, 6);
 
             switch (menuSelection) {
                 case 1:
-                    io.print("LIST STUDENTS");
+                    io.print("DISPALU ORDER");
                     break;
                 case 2:
-                    io.print("CREATE STUDENT");
+                    io.print("ADD ORDER");
                     break;
                 case 3:
-                    io.print("VIEW STUDENT");
+                    io.print("EDIT ORDER");
                     break;
                 case 4:
-                    io.print("REMOVE STUDENT");
+                    io.print("REMOVE ORDERT");
                     break;
                 case 5:
+                    io.print("EXPORT ALL DATA");
+                    break;
+                case 6:
                     keepGoing = false;
                     break;
                 default:
