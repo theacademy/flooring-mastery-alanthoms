13241b2c7985192572aff1725a170133b5c9dd46
Alan Thomas <alanthomas171@gmail.com>
2026-05-07 14:34:26 +0100

    Commit 5/ Feat/ Add Order, Tax and Product DTOs

---
+++ flooring-order-management/src/main/java/flooring/dto/Product.java
@@ -0,0 +1,34 @@
+package flooring.dto;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private String productType;
+    private BigDecimal costPerSquareFoot;
+    private BigDecimal laborCostPerSquareFoot;
+
+    public Product(String productType) {
+        this.productType = productType;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public BigDecimal getCostPerSquareFoot() {
+        return costPerSquareFoot;
+    }
+
+    public void setCostPerSquareFoot(BigDecimal costPerSquareFoot) {
+        this.costPerSquareFoot = costPerSquareFoot;
+    }
+
+    public BigDecimal getLaborCostPerSquareFoot() {
+        return laborCostPerSquareFoot;
+    }
+
+    public void setLaborCostPerSquareFoot(BigDecimal laborCostPerSquareFoot) {
+        this.laborCostPerSquareFoot = laborCostPerSquareFoot;
+    }
+}
---
+++ flooring-order-management/src/main/java/flooring/dto/Tax.java
@@ -0,0 +1,34 @@
+package flooring.dto;
+
+import java.math.BigDecimal;
+
+public class Tax {
+
+    private String stateAbbreviation;
+    private String stateName;
+    private BigDecimal taxRate;
+
+    public Tax(String stateAbbreviation) {
+        this.stateAbbreviation = stateAbbreviation;
+    }
+
+    public String getStateAbbreviation() {
+        return stateAbbreviation;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
+    }
+
+    public BigDecimal getTaxRate() {
+        return taxRate;
+    }
+
+    public void setTaxRate(BigDecimal taxRate) {
+        this.taxRate = taxRate;
+    }
+}
